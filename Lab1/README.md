3c952e599bd2b828119f83d83f74785e3a83140a
sspatari <spatari.stanislav@gmail.com>
2017-11-13 15:11:00 +0100

    Add getWinner and tendecty to StockExchange class

--- Lab1/README.md
+++ Lab1/README.md
@@ -5,6 +5,6 @@
     2. Execute queries from files *ag_data.sql* & *ag_name.sql*.
     
 ## Connect to database
-    1. Create *ConnectDB.java*, a copy of *ConnectDB.sample.java*.
+    1. Create *ConnectDB.java*, a copy of *ConnectDB.java.sample*.
     2. Change db name user and password for your database.
     3. Test connection to database using commands specified in *makefile*.
--- Lab1/StockExchange.java
+++ Lab1/StockExchange.java
@@ -2,12 +2,10 @@ import java.util.ArrayList;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class StockExchange
-{
+public class StockExchange {
 	private ConnectDB dbHandle;
 
-	public StockExchange()
-	{
+	public StockExchange() {
 		dbHandle = new ConnectDB();
 		// Verbindung zur Datenbank herstellen
 		System.out.println("StockExchange: Opening database");
@@ -16,47 +14,83 @@ public class StockExchange
 		}
 	}
 
-	public void close()
-	{
+	public void close() {
 		System.out.println("StockExchange: Closing database");
 		dbHandle.close();
 	}
 
-	public String[] getAGs()
-	{
+	public String[] getAGs() {
 		ArrayList<String> list = new ArrayList<String>();
 		try {
 			ResultSet res;
 			res = dbHandle.execute("SELECT AG_NAME FROM ag_name");
 			while (res.next())
 				list.add(res.getString(1));
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			System.err.println("SQL error: " + e.getMessage());
 		}
 		return list.toArray(new String[0]);
 	}
-	
-	public double getMarketValue(String ag, int day)
-	{
+
+	public double getMarketValue(String ag, int day) {
 		try {
 			ResultSet res;
 			res = dbHandle.execute("SELECT VALUE "
-								   + "FROM ag_data, ag_name "
-								   + "WHERE ag_data.AG_ID = ag_name.AG_ID AND "
-								   + "AG_NAME = '" + ag + "' AND "
-								   + "DAY = '" + day + "'");
+					+ "FROM ag_data, ag_name "
+					+ "WHERE ag_data.AG_ID = ag_name.AG_ID AND "
+					+ "AG_NAME = '" + ag + "' AND "
+					+ "DAY = '" + day + "'");
 			if (res.next())
 				return res.getDouble(1);
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			System.err.println("SQL error: " + e.getMessage());
 		}
 		return -1.0;
 	}
-	
-	public Depot openDepot()
-	{
+
+	public Depot openDepot() {
 		return new Depot(this);
+	}
+
+	public WinnerInfo getWinner(int d1, int d2) throws StockException {
+		String[] agNames = getAGs();
+		if(agNames.length == 0) {
+			throw new StockException("Test Exeption");
+		}
+		WinnerInfo winnerInfo = new WinnerInfo(agNames[0], ((getMarketValue(agNames[0],d2) - getMarketValue(agNames[0],d1))/getMarketValue(agNames[0],d1)) * 100);
+		for(int i = 1; i < agNames.length; ++i) {
+			double marketValue1 = getMarketValue(agNames[i],d1);
+			double marketValue2 = getMarketValue(agNames[i],d2);
+			double percentageGain = (marketValue2 - marketValue1)/marketValue1 * 100;
+			if(winnerInfo.getPercentageGain() < percentageGain) {
+				winnerInfo.setAgName(agNames[i]);
+				winnerInfo.setPercentageGain(percentageGain);
+			}
+		}
+		return winnerInfo;
+	}
+
+	public double tendence(int d1) throws StockException {
+		int d2 = d1 + 1;
+		double sumD1 = 0, sumD2 = 0 ;
+		String[] agNames = getAGs();
+		for(String agName: agNames) {
+			sumD1 += getMarketValue(agName, d1);
+			sumD2 += getMarketValue(agName, d2);
+		}
+		return (sumD2 - sumD1)/sumD1 * 100;
+	}
+
+
+
+	private class StockException extends Exception {
+		String message;
+
+		public StockException(String message) {
+			this.message = message;
+		}
+		public String toString() {
+			return ("StockException Occurred: " + message) ;
+		}
 	}
 }
